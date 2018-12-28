8b516b0289a102e4ba62bc4550c50aae05985173
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2018-12-28 14:32:50 +0100

    made an entry point

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from setuptools import setup
 
 setup(
     name='XCaliburMethodReader',
+    py_modules=['XCaliburMethodReader'],
     version='0.1',
     url='',
     license='GNU',
     author='Nicholas A. Del Grosso',
     author_email='delgrosso.nick@gmail.com',
-    description='A command-line program for mass spectrometry researchers that extracts and converts data from Thermo XCalibur Method (.meth) files.'
+    description='A command-line program for mass spectrometry researchers that extracts and converts data from Thermo XCalibur Method (.meth) files.',
+    entry_points="""
+        [console_scripts]
+        XCaliburMethodReader=XCaliburMethodReader:main
+    """
 )
